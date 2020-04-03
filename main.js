34c82b564e5eb91ed3e546218a26373756b13ccd
Jan <jlbichel@gmail.com>
2020-04-03 23:33:05 +0200

    version 0.1

---
+++ main.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const app = express();
+
+function log(req, res, next){
+    //console.log(parseInt(req.body.fname) + 1);
+    res.send("danke" + parseInt(req.body));
+    console.log(req)
+}
+
+app.use(bodyParser.urlencoded())
+app.use(express.static("public"));
+app.use("/test", log);
+app.listen(3000, () => console.log("listening at 3000"));
