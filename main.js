bdd3c03dc7eba5cda0d295ef86723ae458e57246
Steffen <steffen@indigo-nms.eu>
2020-04-04 13:58:49 +0200

    Make example work again, prevent warning

--- main.js
+++ main.js
@@ -4,20 +4,20 @@ const app = express();
 
 function log(req, res, next) {
     //console.log(parseInt(req.body.fname) + 1);
-    res.send("danke" + parseInt(req.body));
+    res.send("got from you: " + parseInt(req.body.fname));
     console.log(req)
 }
 
 app.use(express.static("public"));
-app.use(bodyParser.urlencoded())
+app.use(bodyParser.urlencoded({ extended: true }))
 app.use((req, res, next) => {
     // if req is nicht erlaubt
     // dann respond with error
     // else
-    // next()
+    next()
 })
 app.use("/test", log);
-app.listen(3000, () => console.log("listening at 3000"));
+app.listen(3000, () => console.log("listening at 3000\nhttp://localhost:3000/"));
 
 app.use('/user', require('./modules/user'));
 app.use('/game', require('./modules/game'));
