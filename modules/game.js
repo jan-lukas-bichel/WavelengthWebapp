04b9492d2f6286885a45c387d4ab93d5fc346643
Jan <jlbichel@gmail.com>
2020-04-04 16:19:47 +0200

    Added Join Game and Set Name Functions

--- modules/game.js
+++ modules/game.js
@@ -1,18 +1,80 @@
 const express = require('express');
 const router = express.Router();
 
-router.use('/new', createGame);
+router.use('/join', joinGame);
+router.use('/newGame', startGame);
+router.use('/name', setName);
+router.use('/number', getNumber);
+router.use('/newWord', setNewWord);
+router.use('/word', getWord);
+router.use('/scale', getScale);
+router.use('/scores', getScores);
+router.use('/players', getPlayers);
+router.use('/answer', confirmAnswer);
+module.exports = router;
+
+const IDLength = 10;
+const players = [];
+const gameInProgress = false;
+
+// Gibt den Index eines Spielers aus dem Players-Array anhand der ID zurück. Wenn kein Spieler mit der ID existiert, wird -1 zurückgegeben.
+function playerIndexByID(ID) {
+    indexNumber = 0;
+
+    players.forEach(player => {
+        if (player.id == ID) {
+            return indexNumber;
+        }
+        else if (indexNumber == players.length) {
+            return -1;
+        }
+        else {
+            indexNumber += 1;
+        }
+    });
+}
+
+
+function generateID(idLength) { // TO DO: Ganze Funktion austauschen
+    var result = '';
+    var characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    var charactersLength = characters.length;
+
+    for (var i = 0; i < idLength; i++) {
+        result += characters.charAt(Math.floor(Math.random() * charactersLength));
+    }
+    if (playerIndexByID(result) >= 0) {
+        result = generateID(idLength);
+    }
+    return result;
+}
 
 function joinGame(req, res, next) {
-    // checken ob schon eine Runde läuft, wenn ja die anfrage ignorieren (später evtl. anderes handling, wie zB spectating)
-    // Sonst checken, ob der Spieler schon eine ID hat, wenn ja anfrage ignorieren
-    // Sonst eine ID zuordnen
+    const reqID = 0; // TO DO: player ID aus dem request Header lesen und zuweisen
+
+    if (gameInProgress) {
+        return;
+    }
+    else if (playerIndexByID(reqID) >= 0) {
+        return;
+    }
+    else {
+        players.push = { id: generateID(IDLength), name: "", team: 0 };
+    }
 }
 
 function setName(req, res, next) {
-    // checken ob schon eine Runde läuft, wenn ja die anfrage ignorieren
-    // Sonst checken, ob der Spieler schon eine ID hat, wenn nicht, dann auf joinGame weiterleiten
-    // Sonst der Spieler ID den gesendeten Namen zurordnen
+    const newName = ""; // TO DO: neuen Namen aus der Request lesen
+    const reqID = 0; // TO DO: reqID aus Request Header lesen
+    const playerIndex = playerIndexByID(reqID);
+
+    if (gameInProgress) {
+        return;
+    }
+    if (playerIndex < 0) {
+        // joinGame(req, res, next); <- funktioniert das so?
+    }
+    players[playerIndex][name] = newName; // <- Keine Ahnung ob diese Syntax richtig ist
 }
 
 function startGame(req, res, next) {
@@ -21,14 +83,14 @@ function startGame(req, res, next) {
     // Sonst neue Runde Initialisieren
     // Score auf 0 setzen
     // Die spieler in Teams einteilen
-        //Immer abwechselnd Team 1 und Team 2 zuordnen, sonderfall bei zwei oder drei spielern: alle ins gleiche Team
+    //Immer abwechselnd Team 1 und Team 2 zuordnen, sonderfall bei zwei oder drei spielern: alle ins gleiche Team
     // Startspieler festlegen
     // Skala für erste Runde festlegen
     // Nummer für erste Runde festlegen
     // Anzahl der Runden festlegen
 }
 
-function getNewNumber(req, res, next) {
+function getNumber(req, res, next) {
     // Aktuell zu erratende Nummer zurückgeben
 }
 
@@ -36,7 +98,7 @@ function setNewWord(req, res, next) {
     // Wort, dass die entsprechende Zahl auf der Skala beschreiben soll, festlegen
 }
 
-function getNewWord(req, res, next) {
+function getWord(req, res, next) {
     // Festgelegtes Wort der Runde zurückgeben
 }
 
@@ -56,7 +118,7 @@ function getRoundNumber(req, res, next) {
     // Zurückgeben, die wievielte Runde aktuell gespielt wird
 }
 
-function confirmNumber(req, res, next) {
+function confirmAnswer(req, res, next) {
     // Punkte für die Runde daran berechnen, wie dicht die geratene Nummer an der tatsächlichen Nummer war
     // Gesamtscores aktualisieren
     // Anzahl der gespielten Runden aktualisieren
@@ -64,6 +126,3 @@ function confirmNumber(req, res, next) {
     // Eventuell das Gewinnerteam bestimmen, Spiel serverseitig als beendet erklären
     // Sonst neue Skala und neue Nummer und neuen Spieler zum Ausdenken des Wortes festlegen
 }
-
-module.exports = router;
-
