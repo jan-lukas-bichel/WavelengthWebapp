841a46725b429dc4a2df6860671abc95591ce974
Jan <jlbichel@gmail.com>
2020-04-05 22:38:46 +0200

    Added Vue App

--- main.js
+++ main.js
@@ -2,40 +2,15 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const app = express();
 
-function log(req, res, next) {
-    //console.log(parseInt(req.body.fname) + 1);
-    res.send("got from you: " + parseInt(req.body.fname));
-    console.log(req)
-}
-
-app.use(express.static("public"));
-app.use(bodyParser.urlencoded({ extended: true }))
-app.use((req, res, next) => {
+app.use(express.static("vue_app/public/old_index.html"));
+// app.use(bodyParser.urlencoded({ extended: true }))
+/*app.use((req, res, next) => {
     // if req is nicht erlaubt
     // dann respond with error
     // else
     next()
-})
-app.use("/test", log);
-app.listen(3000, () => console.log("listening at 3000\nhttp://localhost:3000/"));
+})*/
 
-app.use('/user', require('./modules/user'));
+//app.use("/game/join", joinGame);
 app.use('/game', require('./modules/game'));
-
-/*
-{
-    routes: [
-        {
-            path: '/user'
-            module: require('./modules/user')
-            subroutes: [
-                path: '/new',
-
-            ]
-        }, // user
-        {
-
-        } // game
-    ]
-}
-*/
+app.listen(3000, () => console.log("listening at 3000\nhttp://localhost:3000/"));
