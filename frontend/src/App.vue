c9aa6d71c6d7e5502dacb93ea957773d8adbc0f6
Jan <jlbichel@gmail.com>
2020-07-16 20:43:30 +0200

    Added Toolbar

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,9 +1,15 @@
 <template>
-<v-app>
-  <p>
-    <router-link to="/">Home</router-link>
-    <router-link to="/game">Game</router-link>
-  </p>
-
-  <router-view></router-view>
-</v-app>
+  <v-app>
+    <v-app-bar app color="indigo" dark>
+      <v-toolbar-title>On a Scale of Ten: The Game</v-toolbar-title>
+      <v-spacer></v-spacer>
+      <v-btn light>
+        <router-link to="/">Home</router-link>
+      </v-btn>
+      <v-btn light>
+        <router-link to="/game">Game</router-link>
+      </v-btn>
+    </v-app-bar>
+    <router-view></router-view>
+  </v-app>
+</template>
